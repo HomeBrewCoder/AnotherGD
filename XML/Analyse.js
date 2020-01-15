9da8aafe82d6458b81812ed1c056e00e649ef741
HomeBrewCoder <57724447+HomeBrewCoder@users.noreply.github.com>
2020-01-15 14:15:56 +0100

    Create Analyse.js

---
+++ XML/Analyse.js
@@ -0,0 +1,25 @@
+placeholder = "<?xml version="1.0"?>" +
+"<level>" +
+"<meta>" +
+"<title>Just a test</title>" +
+"<isDemon>0</isDemon>" +
+"<difficulty>1</difficulty>" +
+"</meta>" +
+"<level>" +
+"<block>" +
+"<type>Spike</type>" +
+"<XPos>10</XPos>" +
+"<YPos>0</YPos>" +
+"</block>" +
+"</level>" +
+
+
+function analyse(file){
+
+
+}
+
+function readData(tag1,tag2){
+var tag0 = "xml"
+
+}
